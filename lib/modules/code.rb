8fd131a144d9379df06b7bd1114ab4f61d54244d
Bertille <aranelle@myopera.com>
2013-04-03 13:56:16 +0300

    code → caesar

--- lib/modules/code.rb
+++ lib/modules/code.rb
@@ -1,24 +1,24 @@
 # -*- encoding : utf-8 -*-
 module Linael
 
-  class Modules::Code < ModuleIRCName="code"
+  class Modules::Caesar < ModuleIRCName="caesar"
   
     Help=[
-      "Module: Code",
+      "Module: Caesar",
       " ",
       "=====Fonctions=====",
-      "!code [string you want to code] => display your string, replacing each letter with the one following it in the alphabet",
+      "!caesar [string you want to code] => display your string, replacing each letter with the one following it in the alphabet",
       " ",
       "=====Options=====",
-      "!code -s[a-z] -t[a-z]",
-      "      -s    : Source letter (default: a)",
-      "      -t    : Target letter (default: b)"
+      "!caesar -s[a-z] -t[a-z]",
+      "        -s    : Source letter (default: a)",
+      "        -t    : Target letter (default: b)"
       ]
     def startMod
-      add_module :cmd => [:code]
+      add_module :cmd => [:caesar]
     end
     def code privMsg
-      if Options.code? privMsg.message
+      if Options.caesar? privMsg.message
         options = Options.new privMsg
         gap = options.target.index - options.source.index
         answer(privMsg,options.all.split(//).map{|c| AlphabetArray[c + gap]})
@@ -28,12 +28,12 @@ module Linael
     AlphabetArray = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
     
     class Options < ModulesOptions
-      generate_to_catch :code => /^!code\s/
+      generate_to_catch :caesar => /^!caesar\s/
       generate_meth :name => "source", :regexp => /\s-s(a-z)\s/, :default => "a"
       generate_meth :name => "target", :regexp => /\s-t(a-z)\s/, :default => "b"
       
       def all
-        @message.slice /^!code -s([a-z]) -t([a-z]) /
+        @message.slice /^!caesar -s([a-z]) -t([a-z]) /
       end
     end
   end
