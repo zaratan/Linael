941d6ce49ea6f4fb3147325973f84f9c568ef927
Zaratan <denis.pasin@gmail.com>
2013-04-05 02:08:34 +0200

    save of the save module
    not TESTED!

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'hpricot'
 gem 'htmlentities'
+gem 'ya2yaml'
--- lib/modules/save.rb
+++ lib/modules/save.rb
@@ -1,144 +1,78 @@
 # -*- encoding : utf-8 -*-
-#encoding: utf-8
 
+require 'yaml'
+require 'ya2yaml'
 
 class Modules::Save < ModuleIRC
 
-Name="save"
+  Name="save"
 
-def startMod
-	addAuthCmdMethod(self,:whiteList,":whiteList")
-	addAuthCmdMethod(self,:blackList,":blackList")
-end
+  def startMod
+  end
 
-def endMod
-	delAuthCmdMethod(self,":whiteList")
-	delAuthCmdMethod(self,":blackList")
-end
+  def self.require_auth
+    true
+  end
 
-def self.requireAuth?
-	true
-end
+  def self.required_mod
+    ["module","blacklist"]
+  end
 
-def self.requiredMod
-	["module","admin"]
-end
+  def save privMsg
+    if Options.save? privMsg.message
+      options = Options.new privMsg
 
+      #list of module to save
+      list_to_save=[]
+      #module to rules them all
+      mod_mod = mod("module")
+      
+      #fill list_to_save
+      if options.what
+        list_to_save << options.what if mod(options.what)
+      else
+        list_to_save = mod_mod.module.map{|mod| mod.class.Name}
+      end
 
+      #if list_to_save empty => error
+      if list_to_save.empty?
+        talk(privMsg.who,"Sorry there is no module to save :(")
+        return
+      end
 
-def whiteList privMsg
-	
-	if (Options.whiteList? privMsg.message)
-		options = Options.new privMsg
-		modModule = getModule("module")
-		modAdmin = getModule("admin")
-		if (modModule.instance.modules.has_key?(options.module))
-			mod=modModule.instance.modules[options.module]
-			if (options.add?)
-				if (options.all?)
-					modAdmin.instance.chan.each do |chan| 
-						talk(privMsg.who,"The chan #{chan} have been added to the whitelist of the module #{options.module}")
-						mod.whiteList(chan)
-					end
-					
-				else
-					mod.whiteList(options.chan)	
-					talk(privMsg.who,"The chan #{options.chan} have been added to the whitelist of the module #{options.module}")
-				end
-			end
-			if (options.del?)
-				if (options.all?)
-					modAdmin.instance.chan.each do |chan| 
-						mod.unWhiteList(chan)
-						talk(privMsg.who,"The chan #{chan} have been deleted from the whitelist of the module #{options.module}")
-					end
-				else
-					mod.unWhiteList(options.chan)
-					talk(privMsg.who,"The chan #{options.chan} have been deleted from the whitelist of the module #{options.module}")
-				end
-			end
-		else
+      #if not present create dir Linael/save
+      Dir.mkdir('../../save',755) unless Dir.exist? ('../../save')
 
-			answer(privMsg,"The module #{options.module} is not loaded :(")
-		end
-	end
-	
-end
-
-def blackList privMsg
-
-	if (Options.blackList? privMsg.message)
-		options = Options.new privMsg
-		modModule = getModule("module")
-		modAdmin = getModule("admin")
-		if (modModule.instance.modules.has_key?(options.module))
-			mod=modModule.instance.modules[options.module]
-			if (options.add?)
-				if (options.all?)
-					modAdmin.instance.chan.each do |chan| 
-						talk(privMsg.who,"The chan #{chan} have been added to the blacklist of the module #{options.module}")
-						mod.blackList(chan)
-					end
-					
-				else
-					mod.blackList(options.chan)	
-					talk(privMsg.who,"The chan #{options.chan} have been added to the blacklist of the module #{options.module}")
-				end
-			end
-			if (options.del?)
-				if (options.all?)
-					modAdmin.instance.chan.each do |chan| 
-						mod.unBlackList(chan)
-						talk(privMsg.who,"The chan #{chan} have been deleted from the blacklist of the module #{options.module}")
-					end
-				else
-					mod.unBlackList(options.chan)
-					talk(privMsg.who,"The chan #{options.chan} have been deleted from the blacklist of the module #{options.module}")
-				end
-			end
-		else
-
-			answer(privMsg,"The module #{options.module} is not loaded :(")
-		end
-	end
-end
+      #for each mod to save
+      list_to_save.each do |mod_name|
+        #ya2yamlize the mod (with begin for strange mod) 
+        begin
+          to_write = mod(mod_name).ya2yaml
+        rescue Exception
+          talk(privMsg.who,"Sorry, i can't save module #{mod_name}... It's too complicated ><")
+        end
+        if to_write
+          #write a single file in Linael/save named #{module_name}.yaml
+          File.delete("#{mod_name}.yaml") if File.exist?("#{mod_name}.yaml")
+          File.open("#{mod_name}.yaml") do |file|
+            file.write to_write
+          end
+          #say everything is fine in private
+          talk(privMsg.who,"Everything is perfect \o/ #{mod_name} is saved!")
+        end
+      end
+    end
+  end
 
 
 
-end
 
-class Modules::Blacklist::Options
 
-	def initialize privMsg
-		@message = privMsg.message
-	end
-
-	def add?
-		@message =~ /\s-add\s/
-	end
-
-	def del?
-		@message =~ /\s-del\s/
-	end
-
-	def self.blackList? message
-		message =~ /^!blacklist\s|^!bl\s/
-	end
-
-	def self.whiteList? message
-		message =~ /^!whitelist\s|^!wl\s/
-	end
-
-	def all?
-		@message =~ /\s-all\s/
-	end
-
-	def module
-		$~[1] if @message =~ /\s([A-z0-9]*)\s/
-	end
-
-	def chan
-		$~[1] if @message =~ /\s(#\S*)\s/
-	end
+  class Options < ModulesOptions
+    generate_to_catch :save => /^!save\s|^!save\r|^!save\n/
+                      :load => /^!load\s|^!load\r|^!load\n/
+    
+    generate_what
+  end
 
 end
