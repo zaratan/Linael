2c08d04b25ebac594cbd4e38e2d4e9fd4e62f161
Zaratan <denis.pasin@gmail.com>
2013-04-20 15:53:00 +0200

    user management

--- lib/modules/link.rb
+++ lib/modules/link.rb
@@ -33,23 +33,47 @@ module Linael
     end
 
     def add_link priv_msg
+      if Options.add_link? priv_msg.message and user.include?(priv_msg.who.downcase)
+        options = Options.new priv_msg
 
+      end
     end
 
     def del_link priv_msg
+      if Options.del_link? priv_msg.message and user.include?(priv_msg.who.downcase)
+        options = Options.new priv_msg
 
+      end
     end
 
     def link priv_msg
+      if Options.link? priv_msg.message
+        options = Options.new priv_msg
+        
+      end
+    end
 
+    def links priv_msg
+      if Options.links? priv_msg.message
+        options = Options.new priv_msg
+        
+      end
     end
 
     def add_user_link priv_msg
-
+      if Options.add_user_link? priv_msg.message
+        options = Options.new priv_msg
+        users << options.who.downcase unless users.include? options.who.downcase
+        answer(priv_msg,"Oki doki! #{options.who.downcase.capitalize} can now link :)")
+      end
     end
 
     def del_user_link priv_msg
-
+      if Options.del_user_link? priv_msg.message
+        options = Options.new priv_msg
+        users.delete options.who.downcase
+        answer(priv_msg,"Oki doki! #{options.who.downcase.capitalize} will no longer link anything")
+      end
     end
 
 
@@ -57,10 +81,11 @@ module Linael
       generate_to_catch :add_link      => /^!link\s*-add\s*\S*\s*\S*/,
                         :del_link      => /^!link\s*-del\s*\S*\s*\S*/,
                         :link          => /^!link\s*[^-\s]\S*\s/,
+                        :links         => /^!links\s*\S*/,
                         :add_user_link => /^!link_user\s*-add\s/,
                         :del_user_link => /^!link_user\s*-del\s/
 
-      generate_value    :link          => /^!link\s*([^-\s?][^?\s]*)\??/,
+      generate_value    :link          => /^!link[s]?\s*([^-\s?][^?\s]*)\??/,
                         :id            => /^!link\s*-\S*\s*([^-\s?][^?\s]*)\s/,
                         :value         => /^!link\s*-\S*\s*\S*\s*(\S*)/
 
