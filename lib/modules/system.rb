b903d7ea0e6cd6a3bf49791c00b2595405d5747e
Zaratan <denis.pasin@gmail.com>
2013-04-20 17:25:20 +0200

    end of bash

--- lib/modules/system.rb
+++ lib/modules/system.rb
@@ -13,10 +13,23 @@ module Linael
     ]
 
     def startMod
+      add_module :cmdAuth => [:bash]
+    end
+
+    def bash priv_msg
+      if Options.bash? priv_msg.message
+        options = Options.new priv_msg
+        result = `#{options.all}`
+        answer(priv_msg,"#{priv_msg.who}: Everything have gone as planned!")
+        result.gsub('\r','').split("\n").each do |line|
+          talk(priv_msg.who,line)
+        end
+      end
     end
 
     class Options < ModulesOptions
-
+      generate_to_catch :bash => /^!bash\s+\S/
+      generate_all
     end
   end
 end
