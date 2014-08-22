2685378a2069b6ec10f6f031b9c7d659fee764e1
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2014-08-22 07:04:13 -0400

    Clean up des méthodes.

--- lib/modules/web/bible.rb
+++ lib/modules/web/bible.rb
@@ -7,28 +7,28 @@ require 'nokogiri'
 
 linael :bible do
 
-  def generate_link(booknum, book, chapter, verse)
+  def generate_link(options)
     str = "http://www.kingjamesbibleonline.org/"
-	if booknum.nil?
-		str += booknum + '-'
+	if options.booknum.nil?
+		str += options.booknum + '-'
 	end
-	str += book 
-	str += '-' + chapter + '-' + verse
+	str += options.book 
+	str += '-' + options.chapter + '-' + options.verse
   end
 
-  def get_text(booknum, book, chapter, verse)
-    link = generate_link(booknum, book, chapter, verse)
+  def get_text(options)
+    link = generate_link(options)
     html = Nokogiri::HTML(open(link))
     html.css('h2')[1].text
   end
 
   on :cmd, :bible, /^!bible\s/ do |msg,options|
-    text = get_text(options.booknum, options.book, options.chapter, options.verse)
+    text = get_text(options)
     answer(msg, text)
   end
 
-  value	:booknum	=> /!bible\s*(\d?)/,
-		:book		=> /!bible\s*\d?\s*(\S*)/,
-		:chapter	=> /!bible\s*\d?\s*\S*\s*(\d*)/,
-		:verse		=> /!bible\s*\d?\s*\S*\s*\d*\s*(\d*)/
+  value	booknum: /!bible\s*(\d?)/,
+        book:    /!bible\s*\d?\s*(\S*)/,
+        chapter: /!bible\s*\d?\s*\S*\s*(\d*)/,
+        verse:   /!bible\s*\d?\s*\S*\s*\d*\s*(\d*)/
 end
